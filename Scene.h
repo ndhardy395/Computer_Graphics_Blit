2b558b8245faeef4140f578d962bb484ccf8c090
Noah <notredamehardy@gmail.com>
2026-09-07 19:24:55 -0400

    Added a TownAtlas namespace that can be used in Scene and main and has all the sprites/images that I got using scripting

--- Scene.h
+++ Scene.h
@@ -1,1 +1,28 @@
 #pragma once
+#include "SpriteAtlas.h"
+
+class Surface;
+
+namespace scene
+{
+	constexpr int FIRE_X = 186;
+	constexpr int FIRE_Y = 186;
+	constexpr unsigned int FIRE_SIZE = 128;
+}
+
+struct Placement
+// signed placement; Blit clips it.
+{
+	SpriteRect sprite;
+	int x;
+	int y;
+};
+
+bool RectsOverlap(int _ax, int _ay, int _aw, int _ah, int _bx, int _by, int _bw, int _bh);
+
+// counts the hand-placed town elements
+const Placement* TownLayout(unsigned int& _outCount);
+
+unsigned int ScatterRandom(Placement* _results, unsigned int _count, unsigned int _screenW, unsigned int _screenH);
+
+void DrawPlacement(Surface& _target, const Surface& _sheet, const Placement* _placements, unsigned int _count);
--- SpriteAtlas.h
+++ SpriteAtlas.h
@@ -10,5 +10,29 @@ struct SpriteRect
 
 namespace TownAtlas
 {
+	//Grass
+	const SpriteRect GRASS_TILE = { 288,128,16,16 };
 
+	//Buildings
+	const SpriteRect SHOP =			{ 16, 16, 96, 83 };
+	const SpriteRect RED_HOUSE =	{ 128, 32, 64, 67 };
+	const SpriteRect GREEN_HOUSE =  { 208, 32, 95, 65 };
+	const SpriteRect STONE_HALL =	{ 16, 112, 144, 72 };
+
+	//Scenery
+	const SpriteRect TREE =			{ 320, 16, 64, 80 };
+	const SpriteRect FENCE =		{ 212, 112, 40, 48 };
+	const SpriteRect GRASS_PATCH =  { 272, 112, 64, 64 };
+	const SpriteRect RED_BUSH =		{ 176, 112, 16, 24 };
+
+	//Wooden signs
+	const SpriteRect SIGN_A = { 176, 176, 16, 16 };
+	const SpriteRect SIGN_B = { 192, 176, 16, 16 };
+	const SpriteRect SIGN_C = { 208, 176, 16, 16 };
+
+	//Bushes
+	const SpriteRect BUSH_A =		{ 16, 192, 16, 16 };
+	const SpriteRect BUSH_B =		{ 32, 192, 16, 16 };
+	const SpriteRect FLOWER_BLUE =  { 48, 192, 16, 16 };
+	const SpriteRect FLOWER_PINK =  { 64, 192, 16, 16 };
 }
